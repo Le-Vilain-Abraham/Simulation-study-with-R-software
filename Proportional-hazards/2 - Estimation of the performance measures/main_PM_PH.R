0ae646adc448f882aeff6aefda98d6a96d6d5f9c
Le-Vilain-Abraham <107055922+Le-Vilain-Abraham@users.noreply.github.com>
2022-07-19 11:57:26 +0200

    Add files via upload

---
+++ Proportional-hazards/2 - Estimation of the performance measures/main_PM_PH.R
@@ -0,0 +1,25 @@
+########################################################################################################################################################## 
+##############################      Code used in "Restricted mean survival time to estimate an intervention effect     ################################### 
+##############################                  in a cluster randomized trial" Le Vilain--Abraham et al.               ################################### 
+##########################################################################################################################################################
+
+#_______________________________________________________ Code for proportional hazards assumption _______________________________________________________#
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------# 
+#------------------------------------------------------ 2 - Estimation of the performance measures ------------------------------------------------------#  
+#--------------------------------------------------------------------------------------------------------------------------------------------------------# 
+
+# R functions 
+##########################################################################################################################################################
+setwd("~/your/path/to/Rfiles/") # set directory where you saved the R files with the necessary functions for the simulation
+source("pm_estimation_PH.R")
+source("performance_measures.R")
+source("true_rmst_difference_PH.R")
+source("survival_function_PH.R")
+
+# Estimation of the performance measures 
+##########################################################################################################################################################
+# set the directory and the name of the file where all the estimations (difference in RMST, variance, 95% confidence interval) were saved in step 1
+dataset <- read.table("~/your/path/to/analysis/name_of_your_file.txt", sep = " ", dec = ".",  header=T)
+
+pm_estimation_PH(dataset)
