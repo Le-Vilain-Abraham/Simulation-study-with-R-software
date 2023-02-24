8161d01ee952811ecf08ef8a367056a7ceb83e53
Le-Vilain-Abraham <107055922+Le-Vilain-Abraham@users.noreply.github.com>
2023-02-24 11:21:56 +0100

    Update main_DA_NPH.R

--- Non-proportional-hazards/1 - Generation of the datasets and statistical analyis/main_DA_NPH.R
+++ Non-proportional-hazards/1 - Generation of the datasets and statistical analyis/main_DA_NPH.R
@@ -41,41 +41,41 @@ table_parameter <- data.frame("D" = 1000,          #number of simulated dataset
 t_star <- 365 
 
 
-# Generation of the data 
+# Generation of the data and analysis
 ##########################################################################################################################################################
-	### Create file to save data 
-  setwd("~/your/path/to/Simulated_datasets/") # set directory where the simulated datasets and the estimations will be saved
-  name.file <- paste("K=", table_parameter[ , "K"], 
-                     "-m=", table_parameter[ , "m"], 
-                     "-tau=", ifelse(table_parameter[ , "gamma"] == 0, 0, 1/(1+2*table_parameter[ , "gamma"])), 
-                     "-HR=", exp(table_parameter[ , "beta"]),
-                     "-t_delay=", table_parameter[ , "t_delay"],
-                     "-censoring=",table_parameter[ , "censoring"], 
-                     sep = "")    
+### Create a file to save data and a folder to save the datasets 
+setwd("~/your/path/to/Simulated_datasets/") # set directory where the simulated datasets and the estimations will be saved
+name.file <- paste("K=", table_parameter[ , "K"], 
+                   "-m=", table_parameter[ , "m"], 
+                   "-tau=", ifelse(table_parameter[ , "gamma"] == 0, 0, 1/(1+2*table_parameter[ , "gamma"])), 
+                   "-HR=", exp(table_parameter[ , "beta"]),
+                   "-t_delay=", table_parameter[ , "t_delay"],
+                   "-censoring=",table_parameter[ , "censoring"], 
+                   sep = "")    
 
-	write.table(data.frame("d" = "d",
-  	                     "K" = "K",
-                         "m" = "m",
-                         "HR" = "HR",
-                         "t_delay" = "t_delay",
-                         "tau" = "tau",
-                         "censoring"="censoring",
-                         "clustering" = "clustering", 
-                         "delta.rmst" = "delta.rmst", 
-                         "var" = "var", "ci.low" = "ci.low", 
-                         "ci.up" = "ci.up",
-                         "method" = "method",
-                         "t_star" = "t_star"), 
+write.table(data.frame("d" = "d",
+                       "K" = "K",
+                       "m" = "m",
+                       "HR" = "HR",
+                       "t_delay" = "t_delay",
+                       "tau" = "tau",
+                       "censoring"="censoring",
+                       "clustering" = "clustering", 
+                       "delta.rmst" = "delta.rmst", 
+                       "var" = "var", "ci.low" = "ci.low", 
+                       "ci.up" = "ci.up",
+                       "method" = "method",
+                       "t_star" = "t_star"), 
             file= paste(name.file, ".txt", sep = ""), 
             sep = " ", dec = ".",
             col.names = FALSE, row.names = FALSE)
 
-	dir.create(name.file) #create folder for dataset
+dir.create(name.file) #create folder for datasets
 
-	### Parallelisation
-	registerDoParallel(cores = 8) #set the number of cores
-	set.seed(table_parameter[,"seed"])
-	res <- foreach(d = 1: table_parameter[,"D"],
+### Parallelisation
+registerDoParallel(cores = 8) #set the number of cores
+set.seed(table_parameter[,"seed"])
+res <- foreach(d = 1: table_parameter[,"D"],
                .packages = c("pseudo", "gee", "survRM2", "foreach")) %dorng% sim_NPH(K = table_parameter[ , "K"], 
                                                                                      m = table_parameter[ , "m"], 
                                                                                      lambda = table_parameter[ , "lambda"], 
